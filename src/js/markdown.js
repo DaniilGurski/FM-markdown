7dcde0307698ef6e001060431a4bc43e275b0bb2
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-06-07 20:34:48 +0200

    markdown editor, preview toggle

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,94 +1,55 @@
+const { marked }     = require("marked");
+
 const main           = document.querySelector(".main");
 const inputSection   = main.querySelector("#input-section");
 const previewSection = main.querySelector("#preview-section");
 const markdownInput  = inputSection.querySelector("#markdown-input-area");
 const previewBox     = previewSection.querySelector("#preview-box");
-
-const markdownPatterns = {
-    heading: /^#+\s*[^#]/,
-    orderedList: /^\d+\.\s*\S+/,
-}
-const { heading, orderedList } = markdownPatterns;
-
-let textLines        = [];
-let currentListItems = []; 
-let listFound        = false;
-
-
-
-// TODO: move to different file
-function cleanText(text) {
-    return text.replace(/(#+)|(\d+\.)\s*/g, "");
+const markdownStyledElements = {
+    "ol": ["preview-list"],
+    "ul": ["preview-list", "preview-list--unordered"],
+    "blockquote": ["preview-blockquote", "bg-text-block", "bg-text-block--border", "font-weight-bold"], 
+    "a": ["text-underline"],
+    "pre": ["preview-code", "bg-text-block"],
+    "code": ["inline-code"]
 }
 
 
-function createHeading(textLine) {
-    // create a header taking into account the number of hashtags in the string
-    const hashString    = textLine.match(/^#+/)[0]; // returns all hashtags at the beginning of a line of text as a single line
-    const headingLevel  = hashString.length; 
+function styleMarkdown(container) {
+    const markdownElements = Array.from(container.children);
 
-    const headingElement = document.createElement(`h${headingLevel}`);
-    headingElement.textContent = cleanText(textLine);
+    markdownElements.forEach(element => {
+        const tagName = element.tagName.toLocaleLowerCase();
+        console.log(tagName)
 
-    previewBox.appendChild(headingElement)
-}
+        if (Object.keys(markdownStyledElements).includes(tagName)) {
+            const classNames = markdownStyledElements[tagName]; 
+            classNames.forEach(className => {
+                element.classList.add(className)
+            });
 
-
-function createList() {
-    const orderedListElement = document.createElement("ol"); 
-
-    currentListItems.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = cleanText(item);
-
-        orderedListElement.appendChild(li);
-    })
-
-    currentListItems = [];
-
-    return orderedListElement; 
-}
-
-
-function renderTextLine(textLine) {
-    let markdownElement; 
-
-    // define how to render the passed line of text
-    if (textLine.match(heading)) {
-        markdownElement = createHeading(textLine);
-    } 
-
-    if (textLine.match(orderedList) && !listFound) {
-        const index = textLines.indexOf(textLine);
-        const searchArray = textLines.slice(index);
-
-        for (const suspect of searchArray) {
-            if (!suspect.match(orderedList)) {
-                break
-            }
-
-            currentListItems.push(suspect)
-            console.log(currentListItems)
         }
-    }
-
+        if (element.hasChildNodes()) {
+            styleMarkdown(element);
+        }
+    });
 }
 
 
-function renderMarkdown() {
-    previewBox.innerHTML = "";
+// function renderMarkdown(textLines) {
+//     previewBox.innerHTML = "";
 
-
-    // go through each line and display it in the preview box
-    textLines.forEach(textLine => {
-        renderTextLine(textLine)
-    })
-}
+//     textLines.forEach(textLine => {
+//         const markdownElement = marked.parse(textLine);
+//         previewBox.insertAdjacentHTML("beforeend", markdownElement)
+//         styleMarkdown();
+//     })
+// }
 
 
 function writeDownLines() {
-    textLines = markdownInput.value.split("\n");
-    renderMarkdown()
+    previewBox.innerHTML = marked.parse(markdownInput.value);
+    styleMarkdown(previewBox);
 }
 
 
--- src/js/preview.js
+++ src/js/preview.js
@@ -1,6 +1,6 @@
 const main                 = document.querySelector(".main");
-const inputSection         = main.querySelector(".input-section");
-const previewSection       = main.querySelector(".preview-section");
+const inputSection         = main.querySelector("#input-section");
+const previewSection       = main.querySelector("#preview-section");
 
 const previewToggleButtons = main.querySelectorAll(".markdown-section-header__toggle-preview");
 const previewToggle = previewSection.querySelector("[data-toggle]");
@@ -21,7 +21,7 @@ previewToggleButtons.forEach((toggle) => {
 
 
         // If you press the toggle switch inside the input section (on mobile devices), the state of the toggle switch inside the preview section should be opposite.
-        if (!toggle.closest(".preview-section")) {
+        if (!toggle.closest("#preview-section")) {
 
             previewToggle.dataset.toggle = "hide";
             return
